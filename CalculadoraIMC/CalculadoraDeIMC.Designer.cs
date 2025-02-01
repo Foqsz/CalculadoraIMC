2b75c6e5cb014ca599b6a1fa76dd6d0fdb64bd6e
Foqsz <contatovictorvinicius05@gmail.com>
2025-02-01 10:34:38 -0300

    feat: retirando partes do codigo que não estavam sendo utilizados e otimizando.

--- CalculadoraIMC/CalculadoraDeIMC.Designer.cs
+++ CalculadoraIMC/CalculadoraDeIMC.Designer.cs
@@ -29,12 +29,10 @@
         private void InitializeComponent()
         {
             idadeLb1 = new Label();
-            label1 = new Label();
             label2 = new Label();
             label3 = new Label();
             button1 = new Button();
             txtIdade = new TextBox();
-            textBox2 = new TextBox();
             txtAltura = new TextBox();
             txtPeso = new TextBox();
             txtResultado = new Label();
@@ -43,47 +41,34 @@
             // idadeLb1
             // 
             idadeLb1.AutoSize = true;
-            idadeLb1.Location = new Point(12, 30);
+            idadeLb1.Location = new Point(67, 30);
             idadeLb1.Name = "idadeLb1";
             idadeLb1.Size = new Size(36, 15);
             idadeLb1.TabIndex = 0;
             idadeLb1.Text = "Idade";
-            idadeLb1.Click += label1_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 62);
-            label1.Name = "label1";
-            label1.Size = new Size(211, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Como você prefere ser identificado(a)?";
-            label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 98);
+            label2.Location = new Point(39, 69);
             label2.Name = "label2";
             label2.Size = new Size(92, 15);
             label2.TabIndex = 0;
             label2.Text = "Altura (ex.: 1.75)";
-            label2.Click += label1_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 130);
+            label3.Location = new Point(39, 105);
             label3.Name = "label3";
             label3.Size = new Size(91, 15);
             label3.TabIndex = 0;
             label3.Text = "Peso (ex.: 61.23)";
-            label3.Click += label1_Click;
             // 
             // button1
             // 
             button1.BackColor = SystemColors.ActiveCaption;
-            button1.Location = new Point(28, 168);
+            button1.Location = new Point(67, 157);
             button1.Name = "button1";
             button1.Size = new Size(135, 23);
             button1.TabIndex = 1;
@@ -94,25 +79,15 @@
             // txtIdade
             // 
             txtIdade.BackColor = SystemColors.InactiveCaption;
-            txtIdade.Location = new Point(54, 27);
+            txtIdade.Location = new Point(152, 27);
             txtIdade.Name = "txtIdade";
             txtIdade.Size = new Size(74, 23);
             txtIdade.TabIndex = 2;
-            txtIdade.TextChanged += textBox1_TextChanged;
-            // 
-            // textBox2
-            // 
-            textBox2.BackColor = SystemColors.InactiveCaption;
-            textBox2.Location = new Point(229, 59);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(74, 23);
-            textBox2.TabIndex = 2;
-            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // txtAltura
             // 
             txtAltura.BackColor = SystemColors.InactiveCaption;
-            txtAltura.Location = new Point(110, 95);
+            txtAltura.Location = new Point(152, 66);
             txtAltura.Name = "txtAltura";
             txtAltura.Size = new Size(74, 23);
             txtAltura.TabIndex = 2;
@@ -120,22 +95,20 @@
             // txtPeso
             // 
             txtPeso.BackColor = SystemColors.InactiveCaption;
-            txtPeso.Location = new Point(109, 127);
+            txtPeso.Location = new Point(152, 105);
             txtPeso.Name = "txtPeso";
             txtPeso.Size = new Size(74, 23);
             txtPeso.TabIndex = 2;
-            txtPeso.TextChanged += textBox4_TextChanged;
             // 
             // txtResultado
             // 
             txtResultado.AutoSize = true;
             txtResultado.BackColor = SystemColors.ActiveCaption;
-            txtResultado.Location = new Point(28, 218);
+            txtResultado.Location = new Point(72, 206);
             txtResultado.Name = "txtResultado";
             txtResultado.Size = new Size(59, 15);
             txtResultado.TabIndex = 0;
             txtResultado.Text = "Resultado";
-            txtResultado.Click += label1_Click;
             // 
             // CalculadoraDeIMC
             // 
@@ -144,17 +117,14 @@
             ClientSize = new Size(330, 256);
             Controls.Add(txtPeso);
             Controls.Add(txtAltura);
-            Controls.Add(textBox2);
             Controls.Add(txtIdade);
             Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(txtResultado);
             Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(idadeLb1);
             Name = "CalculadoraDeIMC";
             Text = "Calculadora de IMC";
-            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -162,12 +132,10 @@
         #endregion
 
         private Label idadeLb1;
-        private Label label1;
         private Label label2;
         private Label label3;
         private Button button1;
         private TextBox txtIdade;
-        private TextBox textBox2;
         private TextBox txtAltura;
         private TextBox txtPeso;
         private Label txtResultado;
